a49e3bf86f65b840c82d9b5119b2ce9600891167
Dpatelit <dpatel99.it@gmail.com>
2025-08-04 17:29:38 +0530

    print

--- 11_str_replace.php
+++ 11_str_replace.php
@@ -1,10 +1,19 @@
 <?php 
-    $sentence = "we should daily eat pizza, sandwitch and burger and drink cocacola";
+    $subject = ['HTML','CSS','JAVASCRIPT','PHP','WORDPRESS'];
 
-    $bad_words = ['pizza','sandwitch','burger','cocacola'];
-    $good_words = ['apple','banana','mango','coconut water'];
+    $subject_as_string = implode(" ",$subject);
+    echo "<br/> $subject_as_string";
 
-    echo "<br/> before replacement sentence = $sentence";
-    $sentence = str_replace($bad_words,$good_words,$sentence);
-    echo "<br/> after replacement sentence = $sentence";
+    $s1 = join(" ",$subject);
+    echo "<br/> $s1";
+    print("<br/>$s1");
+
+    // $sentence = "we should daily eat pizza, sandwitch and burger and drink cocacola";
+
+    // $bad_words = ['pizza','sandwitch','burger','cocacola'];
+    // $good_words = ['apple','banana','mango','coconut water'];
+
+    // echo "<br/> before replacement sentence = $sentence";
+    // $sentence = str_replace($bad_words,$good_words,$sentence);
+    // echo "<br/> after replacement sentence = $sentence";
 ?>  
